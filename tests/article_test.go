b33245fdd1a54487e2c4cff861b9352e1e89b847
Priyank Shah <priyank.shah217@gmail.com>
2022-07-27 18:47:42 +0530

    Added fixture methods

--- tests/article_test.go
+++ tests/article_test.go
@@ -6,6 +6,7 @@ import (
 	"api_automation/entity/user"
 	"github.com/bxcodec/faker/v3"
 	"github.com/stretchr/testify/suite"
+	"log"
 	"testing"
 )
 
@@ -13,56 +14,53 @@ func TestCustomTestSuite(t *testing.T) {
 	suite.Run(t, new(CustomTestSuite))
 }
 
-func (t *CustomTestSuite) Test_CreateArticle() {
-	// Arrange
-	u := user.
+var (
+	a *article.Article
+	u *user.User
+)
+
+func (t *CustomTestSuite) BeforeTest(suiteName string, testName string) {
+	log.Println("beforeTest: " + suiteName + ":::" + testName)
+	u = user.
 		NewBuilder().
 		SetEmail(faker.Email()).
 		SetPassword("123456").
 		SetUsername(faker.FirstName()).
 		Build()
-	articleTitle := faker.Name()
-	articleBody := faker.Sentence()
-	articleDesc := faker.Sentence()
-	a := article.
+	log.Printf("user: %v \n", u)
+
+	a = article.
 		NewBuilder().
 		SetTagList([]string{"Java", "Python"}).
-		SetTitle(articleTitle).
-		SetDescription(articleDesc).
-		SetBody(articleBody).
+		SetTitle(faker.Name()).
+		SetDescription(faker.Sentence()).
+		SetBody(faker.Sentence()).
 		Build()
+	log.Printf("article: %v \n", a)
+}
 
-	// Act
+func (t *CustomTestSuite) AfterTest(suiteName string, testName string) {
+	log.Println("afterTest: " + suiteName + ":::" + testName)
+}
+
+func (t *CustomTestSuite) Test_CreateArticle() {
+	// Arrange
 	admin := actor.NewAdmin()
 	newUser, err := admin.CreateUser(u)
 	t.Require().Nil(err, "error in creating user")
+
+	// Act
 	response, err := newUser.CreateArticle(a)
 
 	// Assert
 	t.Require().Nil(err, "error in creating article")
-	t.Require().Equal(articleTitle, response.Article.Title, "title does not match")
-	t.Require().Equal(articleBody, response.Article.Body, "body does not match")
-	t.Require().Equal(articleDesc, response.Article.Description, "description does not match")
+	t.Require().Equal(a.Title, response.Article.Title, "title does not match")
+	t.Require().Equal(a.Body, response.Article.Body, "body does not match")
+	t.Require().Equal(a.Description, response.Article.Description, "description does not match")
 }
 
 func (t *CustomTestSuite) Test_UpdateArticle() {
 	// Arrange
-	u := user.
-		NewBuilder().
-		SetEmail(faker.Email()).
-		SetPassword("123456").
-		SetUsername(faker.FirstName()).
-		Build()
-	articleTitle := faker.Name()
-	articleBody := faker.Sentence()
-	articleDesc := faker.Sentence()
-	a := article.
-		NewBuilder().
-		SetTagList([]string{"Java", "Python"}).
-		SetTitle(articleTitle).
-		SetDescription(articleDesc).
-		SetBody(articleBody).
-		Build()
 	admin := actor.NewAdmin()
 	newUser, err := admin.CreateUser(u)
 	t.Require().Nil(err, "error in creating user")
@@ -70,39 +68,20 @@ func (t *CustomTestSuite) Test_UpdateArticle() {
 	t.Require().Nil(err, "error in creating article")
 
 	// Act
-	updatedArticleTitle := faker.Name()
-	updatedArticleDesc := faker.Sentence()
-	updatedArticleBody := faker.Sentence()
-	articleToBeUpdated.Article.Title = updatedArticleTitle
-	articleToBeUpdated.Article.Description = updatedArticleDesc
-	articleToBeUpdated.Article.Body = updatedArticleBody
+	articleToBeUpdated.Article.Title = faker.Name()
+	articleToBeUpdated.Article.Description = faker.Sentence()
+	articleToBeUpdated.Article.Body = faker.Sentence()
 	updateArticle, err := newUser.UpdateArticle(articleToBeUpdated)
 
 	// Assert
 	t.Require().Nil(err, "error in updating article")
-	t.Require().Equal(updatedArticleTitle, updateArticle.Article.Title, "title does not match")
-	t.Require().Equal(updatedArticleDesc, updateArticle.Article.Description, "description does not match")
-	t.Require().Equal(updatedArticleBody, updateArticle.Article.Body, "body does not match")
+	t.Require().NotEqual(a.Title, updateArticle.Article.Title, "title does not match")
+	t.Require().NotEqual(a.Description, updateArticle.Article.Description, "description does not match")
+	t.Require().NotEqual(a.Body, updateArticle.Article.Body, "body does not match")
 }
 
 func (t *CustomTestSuite) Test_DeleteArticle() {
 	// Arrange
-	u := user.
-		NewBuilder().
-		SetEmail(faker.Email()).
-		SetPassword("123456").
-		SetUsername(faker.FirstName()).
-		Build()
-	articleTitle := faker.Name()
-	articleBody := faker.Sentence()
-	articleDesc := faker.Sentence()
-	a := article.
-		NewBuilder().
-		SetTagList([]string{"Java", "Python"}).
-		SetTitle(articleTitle).
-		SetDescription(articleDesc).
-		SetBody(articleBody).
-		Build()
 	admin := actor.NewAdmin()
 	newUser, err := admin.CreateUser(u)
 	t.Require().Nil(err, "error in creating user")
