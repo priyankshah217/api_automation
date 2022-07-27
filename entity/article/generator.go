cd32d5bb8d6f6cccaebcce674c1594f78d39124e
Priyank Shah <priyank.shah217@gmail.com>
2022-07-27 19:17:04 +0530

    change types

--- entity/article/generator.go
+++ entity/article/generator.go
@@ -9,33 +9,33 @@ type IBuilder interface {
 }
 
 type Builder struct {
-	a Article
+	a *Article
 }
 
 func NewBuilder() IBuilder {
 	return &Builder{}
 }
 
-func (b Builder) SetTagList(tagList []string) IBuilder {
+func (b *Builder) SetTagList(tagList []string) IBuilder {
 	b.a.TagList = tagList
 	return b
 }
 
-func (b Builder) SetTitle(title string) IBuilder {
+func (b *Builder) SetTitle(title string) IBuilder {
 	b.a.Title = title
 	return b
 }
 
-func (b Builder) SetDescription(description string) IBuilder {
+func (b *Builder) SetDescription(description string) IBuilder {
 	b.a.Description = description
 	return b
 }
 
-func (b Builder) SetBody(body string) IBuilder {
+func (b *Builder) SetBody(body string) IBuilder {
 	b.a.Body = body
 	return b
 }
 
-func (b Builder) Build() *Article {
-	return &b.a
+func (b *Builder) Build() *Article {
+	return b.a
 }
